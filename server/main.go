cc1859541af9a4293b44e58618ebd3c30244c094
ToniG22 <tonigarces19@gmail.com>
2023-12-04 10:54:14 +0000

    feat: added backend metrics with promql 🙈

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/ansrivas/fiberprometheus/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -19,8 +20,8 @@ type Event struct {
 	Local        string             `bson:"local"`
 	Date         time.Time          `bson:"date"`
 	EventTime    string             `bson:"eventTime"`
-	Participants int32            	`bson:"participants"`
-	Price        float64			`bson:"price"`
+	Participants int32              `bson:"participants"`
+	Price        float64            `bson:"price"`
 }
 
 var db *mongo.Client
@@ -29,6 +30,9 @@ var mongoCtx context.Context
 
 func main() {
 	app := fiber.New()
+	prometheus := fiberprometheus.New("my-service-name")
+	prometheus.RegisterAt(app, "/metrics")
+	app.Use(prometheus.Middleware)
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
 		AllowHeaders: "Origin, Content-Type, Accept",
@@ -99,39 +103,38 @@ func main() {
 	app.Post("/events", func(c *fiber.Ctx) error {
 		log.Println("Create Event")
 		rawBody := c.Body()
-    	log.Printf("Raw Request Body: %s", rawBody)
+		log.Printf("Raw Request Body: %s", rawBody)
 		event := new(Event)
-		
+
 		// Attempt to parse the request body into the Event struct
 		if err := c.BodyParser(&event); err != nil {
 			log.Println("Error parsing request body:", err)
 			return c.Status(400).SendString("Invalid request body")
 		}
-	
+
 		// Insert the event into MongoDB
 		result, err := eventdb.InsertOne(mongoCtx, event)
 		if err != nil {
 			log.Println("Error inserting event into MongoDB:", err)
 			return c.Status(500).SendString("Internal Server Error")
 		}
-	
+
 		// Set the generated ObjectID to the event
 		event.Id = result.InsertedID.(primitive.ObjectID)
-	
+
 		return c.JSON(event)
 	})
 
 	app.Patch("/events/:id", func(c *fiber.Ctx) error {
 		log.Println("Update Event")
-	
+
 		// Get the event ID from the request params
 		id, err := primitive.ObjectIDFromHex(c.Params("id"))
 		if err != nil {
 			log.Println("Error converting ID:", err)
 			return c.Status(400).SendString("Invalid ID")
 		}
-		
-	
+
 		// Create a new Event struct to hold the updated fields
 		updateFields := make(map[string]interface{})
 		log.Println(updateFields)
@@ -150,40 +153,37 @@ func main() {
 			log.Println("Error updating event in MongoDB:", err)
 			return c.Status(500).SendString("Internal Server Error")
 		}
-	
+
 		// Check if any documents were modified
 		if result.ModifiedCount == 0 {
 			return c.Status(404).SendString("Event not found or no changes applied")
 		}
-	
+
 		return c.SendString("Event updated successfully")
 	})
 
 	app.Delete("/events/:id", func(c *fiber.Ctx) error {
 		log.Println("Delete Event")
-	
+
 		id, err := primitive.ObjectIDFromHex(c.Params("id"))
 		if err != nil {
 			return c.Status(400).SendString(err.Error())
 		}
-	
+
 		// Delete the event from MongoDB
 		result, err := eventdb.DeleteOne(mongoCtx, primitive.M{"_id": id})
 		if err != nil {
 			log.Fatal(err)
 			return c.Status(500).SendString("Internal Server Error")
 		}
-	
+
 		// Check if any documents were deleted
 		if result.DeletedCount == 0 {
 			return c.Status(404).SendString("Event not found")
 		}
-	
+
 		return c.SendString("Event deleted successfully")
 	})
-	
-
-	
 
 	log.Fatal(app.Listen(":3000"))
 }
